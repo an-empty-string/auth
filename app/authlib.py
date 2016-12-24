ca8336349d000bac5331c859c6e9e507202bb296
Fox Wilson <foxwilson123@gmail.com>
2016-12-24 11:33:02 -0500

    update authlib

---
+++ app/authlib.py
@@ -0,0 +1,1 @@
+../authlib/authlib.py
--- authlib/authlib.py
+++ authlib/authlib.py
@@ -10,21 +10,17 @@ class SSOAuthenticator:
         if not self.server.startswith("https://"):
             warnings.warn("SSOAuthenticator was not initialized with a secure endpoint")
 
-    def set_callback(self, callback):
-        self.callback = callback
-        self.domain = urllib.parse.urlparse(callback).netloc
+    def request_url(self, callback):
+        req = json.dumps(dict(callback=callback)).encode()
+        return "{}/request/{}/".format(self.server, base64.urlsafe_b64encode(req).decode())
 
-    def request_url(self):
-        req = json.dumps(dict(callback=self.callback)).encode()
-        return "{}/request/{}".format(self.server, base64.urlsafe_b64encode(req).decode())
-
-    def token(self, token):
-        req = requests.get("{}/session/{}".format(self.server, token))
+    def token(self, token, url):
+        req = requests.get("{}/session/{}/".format(self.server, token))
         if req.status_code != 200:
             return False
 
         token = req.json()
-        if token["valid_for"]["domain"] != self.domain:
+        if token["valid_for"]["domain"] != urllib.parse.urlparse(url).netloc:
             return False
 
         return token
